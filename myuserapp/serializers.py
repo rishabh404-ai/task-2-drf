a20eabc89c0fe258ce9b49960b80c07aa78bf0e7
Rishabh <mishrarishabh404it@gmail.com>
2020-10-12 22:34:09 +0530

    Added Serializers Fields Conditions

--- myuserapp/serializers.py
+++ myuserapp/serializers.py
@@ -3,17 +3,31 @@ from .models import UserRegister, UserRecord
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
+    
+    name      = serializers.CharField(required=True)
+    idcard_no = serializers.IntegerField(required=True)
+    id_type   = serializers.CharField(required=True)
+    address   = serializers.CharField(required=True)
+    phone_no  = serializers.CharField(required=True)
+    email     = serializers.EmailField(required=True)
+    meet_with = serializers.CharField(required=True)
+
 
     class Meta:
         model = UserRegister
-        fields = '__all__'
+        fields = ['name','idcard_no','id_type','address','phone_no','email','meet_with']
 
 
 
 
 class UserRecordSerializer(serializers.ModelSerializer):
 
+    name       = serializers.CharField(required=True)
+    entry_time = serializers.CharField(required=True)
+    exit_time  = serializers.CharField(required=True)
+
+
     class Meta:
         model = UserRecord
-        fields = '__all__'
+        fields = ['person','entry_time','exit_time']
 
